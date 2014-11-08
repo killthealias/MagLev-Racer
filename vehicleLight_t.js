f2ff1e007482823cb21ff7338a73d151080f3e3b
killthealias <killthealias@gmail.com>
2014-11-08 11:30:05 -0800

    Modified Light Script
    Commented out "reverse light" code, changed light intensity, commented
    out Debug.Log("nVelocity")

--- vehicleLight_t.js
+++ vehicleLight_t.js
@@ -4,14 +4,16 @@ var vehicle : vehicle_t;
 
 function Update () {
   var nVelocity : float = (vehicle.velocity / (9.81 * vehicle.getMass()));
-  Debug.Log("nVelocity = " + nVelocity);
+ // Debug.Log("nVelocity = " + nVelocity);
 
   if(!vehicle.isInReverse()){
-    light.color = Color(0/255.0, 143.0/255.0, 255.0/255.0, 255.0/255.0);
-    light.intensity = (nVelocity * nVelocity);
+	light.color = Color(0/255.0, 143.0/255.0, 255.0/255.0, 255.0/255.0);
+//   light.intensity = (nVelocity * nVelocity);
+	light.intensity = (nVelocity * nVelocity) * 0.15;
   }
   else{
-    light.color = Color(218.0/255.0, 63.0/255.0, 63.0/255.0, 255.0/255.0);
-    light.intensity = (nVelocity * nVelocity)/0.2;
+//    light.color = Color(218.0/255.0, 63.0/255.0, 63.0/255.0, 255.0/255.0);
+	light.color = Color(0/255.0, 143.0/255.0, 255.0/255.0, 255.0/255.0);
+    light.intensity = (nVelocity * nVelocity) * 0.15;
   }
 }
